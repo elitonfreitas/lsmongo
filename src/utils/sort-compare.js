5fd10f0c6986c51dc2a63bba6dc13f424901f87b
Eliton Freitas <elitonfreitas@gmail.com>
2019-05-07 15:00:13 -0300

    Update sort option
    Make sort option works.

--- src/utils/sort-compare.js
+++ src/utils/sort-compare.js
@@ -1,25 +1,29 @@
 const compare = (sort, a, b, i, fields) => {
 	i = i || 0
 	fields = fields || Object.keys(sort)
-
+  
 	let field = fields[i]
-
+  
 	if (!field) {
-		return 0
+	  return 0
 	}
-
+  
 	if (a[field] === b[field]) {
-		i++
-		return compare(sort, a, b, i, fields)
+	  i++
+	  return compare(sort, a, b, i, fields)
 	}
-
+  
 	if (sort[field] === 1) {
-		return a[field] - b[field]
+	  if (a[field] < b[field]) return -1;
+	  if (a[field] > b[field]) return 1;
+	  return 0;
 	}
-
 	if (sort[field] === -1) {
-		return b[field] - a[field]
+	  if (a[field] > b[field]) return -1;
+	  if (a[field] < b[field]) return 1;
+	  return 0;
 	}
-}
-
-export default compare
+  }
+  
+  export default compare
+  
